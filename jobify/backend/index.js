49f78bdb4aa93f4745bfedc7a71b53ce3aaa7a39
suyashnema0707 <suyashnema0707@gmail.com>
2024-07-01 20:38:45 +0530

    commit

---
+++ jobify/backend/index.js
@@ -0,0 +1,54 @@
+import express from "express";
+import { dbConnection } from "./database/dbConnection.js";
+import jobRouter from "./routes/jobRoutes.js";
+import userRouter from "./routes/userRoutes.js";
+import applicationRouter from "./routes/applicationRoutes.js";
+import { config } from "dotenv";
+import cors from "cors";
+import { errorMiddleware } from "./middlewares/error.js";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+import morgan from 'morgan';
+
+const app = express();
+
+app.use(morgan('dev'));
+
+config({ path: "./config/config.env" });
+import cloudinary from "cloudinary";
+
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUDINARY_CLIENT_NAME,
+  api_key: process.env.CLOUDINARY_CLIENT_API,
+  api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
+});
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    method: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/job", jobRouter);
+app.use("/api/v1/application", applicationRouter);
+dbConnection();
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running at port ${process.env.PORT}`);
+});
+
+app.use(errorMiddleware);
+export default app;
